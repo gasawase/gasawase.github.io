12444a3055ad3db4a687999296cdf23b7198b06b
gasawase <gasawase@mail.uc.edu>
2022-04-20 10:09:14 -0400

    Created gallery that's centered

--- js/main.js
+++ js/main.js
@@ -43,3 +43,4 @@ window.onscroll = function() {scrollFunction()};
                       document.body.scrollTop = 0; // For Safari
                       document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
                 }
+                
